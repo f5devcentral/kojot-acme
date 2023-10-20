5c5dccf77baccfba4fa700ca50dde06a73fb52bf
vkosuri <malli.kv2@gmail.com>
2023-10-20 09:15:01 +0000

    separate irule and enhance better redablity

--- uninstall.sh
+++ uninstall.sh
@@ -6,23 +6,33 @@
 ## Description: Uninstall wrapper for Dehydrated Acme client and all components
 ##
 ## Usage:
-## - Execute: curl -s https://<this-repo-url>/uninstall.sh | bash
+## - Execute: curl -s https://raw.githubusercontent.com/f5devcentral/kojot-acme/uninstall.sh | bash
 
 
-## Delete log file
+# Function to delete a resource if it exists
+delete_resource() {
+    local resource="$1"
+    tmsh delete "$resource" > /dev/null 2>&1
+}
+
+# Function to delete a directory and all its contents
+delete_directory() {
+    local directory="$1"
+    rm -rf "$directory" > /dev/null 2>&1
+}
+
+# Delete log file
 rm -f /var/log/acmehandler > /dev/null 2>&1
 
+# Delete config data group (challenge data group referenced by acme iRule, also not deleted)
+delete_resource "ltm data-group internal dg_acme_config"
 
-## Delete config data group (challenge data group referenced by acme iRule, also not deleted)
-tmsh delete ltm data-group internal dg_acme_config > /dev/null 2>&1
+# Delete iFiles
+delete_resource "sys file ifile f5_acme_account_state"
+delete_resource "sys file ifile f5_acme_config_state"
 
+# Delete /shared/acme folder and all contents
+delete_directory "/shared/acme"
 
-## Delete iFiles
-tmsh delete sys file ifile f5_acme_account_state > /dev/null 2>&1
-tmsh delete sys file ifile f5_acme_config_state > /dev/null 2>&1
-
-
-## Delete /shared/acme folder and all contents
-rm -rf /shared/acme > /dev/null 2>&1
-
+echo "Uninstallation completed successfully."
 exit 0
